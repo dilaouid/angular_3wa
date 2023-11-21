f9906961eb53d5896acfb4eebf9b94911c0aa1e8
dilaouid <dilaouid@student.42.fr>
2023-11-21 21:56:38 +0100

    from class to interface (Pastrie)

--- src/app/pastrie.ts
+++ src/app/pastrie.ts
@@ -1,25 +1,12 @@
-export class Pastrie {
+export interface Pastrie {
     id: string;
     ref: string;
     name: string;
     description: string;
     quantity: number;
     order: number;
-    url?: string;
-    tags?: string[];
     like?: string;
-    choice?: boolean;
-
-    constructor() {
-        this.id = '0';
-        this.ref = '';
-        this.name = '';
-        this.description = '';
-        this.quantity = 0;
-        this.order = 0;
-        this.url = '';
-        this.tags = [];
-        this.like = '0';
-        this.choice = false;
-    }
-}
+    tags?: string[];
+    url?: string;
+    choice? : boolean;
+};
